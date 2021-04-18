3616cb3206f00c5c64a22e5094f87ffd4417a78f
theslantedroom <73557008+theslantedroom@users.noreply.github.com>
2021-04-18 11:07:47 -0700

    push update

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -3,18 +3,22 @@
 //Memo 2n
 const memoFibonacci = (n, memo) => {
   memo = memo || {};
-
+  // if memo[n] exists is has already been calculated, return it
   if (memo[n]) {
+    console.log('meno[n]',memo[n]);
     return memo[n];   
   };
+  // special cases, fib[0] = 0 and fib[1]=1
   if (n <= 1) {
     printAnswerM(n);
     return n;
   }
-
-  let answer = memo[n] = memoFibonacci(n-1, memo) + memoFibonacci(n-2, memo);
+  
+  // memo[n] needs to be calutated if we get here
+  // let answer = memo[n] = memoFibonacci(n-1, memo) + memoFibonacci(n-2, memo);
+  let answer = memoFibonacci(n-1, memo) + memoFibonacci(n-2, memo);
+  memo[n] = answer;
   printAnswerM(answer);
-
   return answer;
 }
 
