022e1ff4eab30be9c1e9e7839f1db07740cb1111
theslantedroom <73557008+theslantedroom@users.noreply.github.com>
2021-04-17 21:01:24 -0700

    push update

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,25 +1,55 @@
 
-var generate = function(n) {
-  fibNums = n;
+
+//Memo 2n
+const memoFibonacci = (n, memo) => {
+  memo = memo || {};
+
+  if (memo[n]) {
+    return memo[n];   
+  };
+  if (n <= 1) {
+    printAnswerM(n);
+    return n;
+  }
+
+  let answer = memo[n] = memoFibonacci(n-1, memo) + memoFibonacci(n-2, memo);
+  printAnswerM(answer);
+
+  return answer;
+}
+
+const memobtn = () => {
+  let n = document.querySelector('#inputM').value
+  memoFibonacci(n);
+}
+function printAnswerM(content) {
+  document.getElementById('memoanswer').innerText = content;    
+}
+
+
+
+
+// recursive 2^n
+const recursiveFibonacci = (n) => {
+  if (n >= 21){
+    document.getElementById('recursiveanswer').innerText = 'that would take too long with this algorithm';    
+    return
+  }
   if ( n <= 1){
-    printAnswer(n);
+    printAnswerR(n);
     return n;  
   } 
-  let answer = generate(n-1)+generate(n-2);
-  printAnswer(answer);
+  let answer = recursiveFibonacci(n-1)+recursiveFibonacci(n-2);
+  printAnswerR(answer);
   return answer;
-  // return fibNums;
 };
 
-
-function printAnswer(content) {
-    document.getElementById('answer').innerText = content;    
+const recursivebtn = () => {
+  let n = document.querySelector('#inputR').value
+  recursiveFibonacci(n);
+}
+function printAnswerR(content) {
+  document.getElementById('recursiveanswer').innerText = content;    
 }
 
-const generatebtn = () => {
-  var parent = document.getElementById('parent');
-  // while ( parent.firstChild ) parent.removeChild( parent.firstChild );
-  let rows = document.querySelector('#input').value
-  // generate(rows);
-  generate(rows);
-}
+
